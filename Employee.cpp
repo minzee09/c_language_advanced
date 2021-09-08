71a08490c3df6f2a776882531dc69c99376d4170
Kim Minji <68258295+minzee09@users.noreply.github.com>
2021-09-09 01:27:58 +0900

    Delete Employee.cpp

--- Employee.cpp
+++
@@ -1,9 +0,0 @@
-#include "Employee.h"
-
-string Employee::getName() { return name; }
-string Employee::getPhone() { return phone; }
-int Employee::getSalary() { return salary; }
-
-void Employee::setName(string n) { name = n; }
-void Employee::setPhone(string p) { phone = p; }
-void Employee::setSalary(int s) { salary = s; }
